e953238675d981dd60fabb786b509e989fc1a152
Eric Bellamy <eric@bellamy.cc>
2023-03-07 22:40:56 -0500

    finished navbar

---
+++ include/js/navigation/menu/mobile.js
@@ -0,0 +1,40 @@
+const navElement = document.querySelector("#nav");
+const navPopoutElement = document.querySelector("#nav .popout");
+const navMobileMenu = document.querySelector("#navMobileMenu");
+const navMobileMenuBackground = document.querySelector("#navMobileMenu .background");
+let navStatus = false;
+function updateNavState(){
+	if(window.getComputedStyle(this.parentNode).display != 'none'){
+		if(!navStatus){
+			navStatus = true;
+
+			const navHeight = navElement.clientHeight;
+			const expandableHeight = window.innerHeight;
+	
+			// Set parameters of expanding nav
+			navMobileMenu.style.display = "block";
+			navMobileMenu.style.top = (navHeight - 1) + "px";
+			navMobileMenu.style.height = (expandableHeight - navHeight + 1) + "px";
+	
+			// Set spacer height to make up for fixed nav
+			navElement.style.height = navHeight + "px";
+			navPopoutElement.style.position = "fixed";
+	
+			setTimeout(function(){
+				if(navStatus) navElement.classList.toggle("expanded", true);
+			}, 50);
+		} else {
+			navStatus = false;
+
+			navElement.style.height = null;			
+			navPopoutElement.style.position = null;
+
+			navMobileMenu.style.display = null;
+			navElement.classList.toggle("expanded", false);
+		}
+	}
+}
+const navBurgers = document.querySelectorAll(".navBurger");
+for(const burger of navBurgers){
+	burger.addEventListener("click", updateNavState);
+}
