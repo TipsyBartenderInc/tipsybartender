616e9d6eecf975bc4e19619956b605b0f14151db
Eric Bellamy <eric@bellamy.cc>
2023-03-07 21:18:46 -0500

    change drink padding & add initial nav menu

--- templates/logic/recipe.js
+++ templates/logic/recipe.js
@@ -206,6 +206,7 @@ module.exports = {
 
 			// Get holiday & recipes
 			let currentHoliday = getCurrentHoliday(currentDate);
+			// currentHoliday = availableHolidays[20];
 			const topRecipes = findMatchingRecipesForTags(templateData.recipe.pages, ruleRecipeTags[currentHoliday.rule]);
 
 			tired.watch.ignoreNext('templates/data/holiday.json');
